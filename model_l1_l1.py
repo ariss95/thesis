f5d2201c4c52d0fbfc98079e0530b972e5c5aaf6
ariss95 <cse32538@cs.uoi.gr>
2021-05-07 20:03:50 +0300

    added activation function of model

--- model_l1_l1.py
+++ model_l1_l1.py
@@ -10,9 +10,9 @@ class l1_l1(nn.Module):
         self.compressed = int(self.input/4)
         self.hidden_size = self.input * 4
         self.device = torch.device('cpu')
-        self.measurement_matrix = np.asarray(
+        self.A_matrix = np.asarray(
             np.random.RandomState().uniform( size=(self.compressed, self.input)), dtype=np.float32)
-        self.matrix_A = torch.tensor(self.measurement_matrix, device=self.device, dtype=torch.float32, requires_grad=True)
+        self.matrix_A = torch.tensor(self.A_matrix, device=self.device, dtype=torch.float32, requires_grad=True)
         dct_dictionary = DCTDictionary(int(sqrt(self.input)),int(sqrt(self.hidden_size)))
         self.Dict_D = torch.tensor(dct_dictionary.matrix, device=self.device, dtype=torch.float32, requires_grad=True)#############FIX
         self.h_0 = torch.zeros((self.batch_size, 2*self.input), device=self.device, dtype=torch.float32,
@@ -24,9 +24,25 @@ class l1_l1(nn.Module):
         self.hidden_layers = 3
 
     def activation_func_phi(u, v, l1, l2, a):
-        
-        phi = torch.zeros()
-        return        
+        g1 = l1/a
+        g2 = l2/a
+        temp_tensor = torch.zeros(u.size(), device="cpu")
+        condition1 = (((v >= 0) & (v+g1+g2 <= u)) | ((v < 0) & (u >= g1 + g2)))
+        temp_tensor[condition1] = u[condition1] - g1 - g2
+        condition2 = (((v >= 0) & (v + g1 - g2 <= u) & (u < v + g1 + g2)) |
+                    ((v < 0) & (v - g1 + g2 > u) & (u >= v - g1 - g2)))
+        temp_tensor[condition2] = v[condition2]
+        condition3 = (((v >= 0) & (g1-g2 <= u) & (v+g1-g2 > u))
+                    | ((v < 0) & (u < v-g1-g2)))
+        temp_tensor[condition3] = u[condition3] - g1 + g2
+        condition4 = (((v >= 0) & (u >= -g1-g2) & (u < g1-g2))
+                    | ((v < 0) & (u >= -g1 + g2)(u < g1+g2)))
+        temp_tensor[condition4] = 0
+        condition5 = ((v>=0) & (u<-g1-g2))
+        temp_tensor[condition5] = u[condition5] + g1 + g2
+        condition6 = ((v<0) & (u<v-g1-g2))
+        temp_tensor[condition6] = u[condition5] - g1 + g2
+        return temp_tensor
 
     def forward(self, data):
         U = (1/self.a)*torch.mm(self.matrix_A.t(), self.Dict_D.t())
